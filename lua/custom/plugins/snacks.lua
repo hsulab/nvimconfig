77d8a90872fdff7942459f71d179e160180097aa
jyxu <ahcigar@foxmail.com>
2025-01-06 15:53:24 -0500

    snacks: move dashboard opts to a separate file

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -1,5 +1,7 @@
+local dashboard = require("custom.snacks.dashboard")
+
 return {
-  'folke/snacks.nvim',
+  "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
   opts = {
@@ -7,81 +9,7 @@ return {
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
-    dashboard = {
-      width = 60,
-      row = nil, -- dashboard position. nil for center
-      col = nil, -- dashboard position. nil for center
-      pane_gap = 4, -- empty columns between vertical panes
-      autokeys = '1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', -- autokey sequence
-      -- These settings are used by some built-in sections
-      preset = {
-        -- Defaults to a picker that supports `fzf-lua`, `telescope.nvim` and `mini.pick`
-        ---@type fun(cmd:string, opts:table)|nil
-        pick = nil,
-        -- Used by the `keys` section to show keymaps.
-        -- Set your custom keymaps here.
-        -- When using a function, the `items` argument are the default keymaps.
-        ---@type snacks.dashboard.Item[]
-        keys = {
-          -- { icon = ' ', key = 'f', desc = 'Find File', action = ":lua Snacks.dashboard.pick('files')" },
-          -- { icon = ' ', key = 'n', desc = 'New File', action = ':ene | startinsert' },
-          -- { icon = ' ', key = 'g', desc = 'Find Text', action = ":lua Snacks.dashboard.pick('live_grep')" },
-          { icon = ' ', key = 'r', desc = 'Recent Files', action = ":lua Snacks.dashboard.pick('oldfiles')" },
-          { icon = ' ', key = 'c', desc = 'Config', action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
-          { icon = ' ', key = 's', desc = 'Session', action = ":lua require('resession').load(nil)" },
-          -- { icon = '󰒲 ', key = 'L', desc = 'Lazy', action = ':Lazy', enabled = package.loaded.lazy ~= nil },
-          { icon = ' ', key = 'q', desc = 'Quit', action = ':qa' },
-        },
-        -- Used by the `header` section
-        header = [[
-
-         /`*-.                    
-          )  _`-.                 
-         .  :_`. .                
-         : _   '  \               
-       _\; *` /_   `*-._          
-        /`-.-'\         `-.       
-           ;       `       `.     
-           :.       .        \    
-           . \  .   :   .-'   .   
-           '  `+.;  ;  '      :   
-           :  '  |    ;       ;-. 
-   ___)    ; '   : :`-:     _.`* ;
- <[bug] .*' /  .*' ; .*`- +'  `*' 
-  *  *   `*-*   `*-*  `*-*'       
-]],
-      },
-      -- item field formatters
-      formats = {
-        icon = function(item)
-          if item.file and item.icon == 'file' or item.icon == 'directory' then
-            return M.icon(item.file, item.icon)
-          end
-          return { item.icon, width = 2, hl = 'icon' }
-        end,
-        footer = { '%s', align = 'center' },
-        header = { '%s', align = 'center' },
-        file = function(item, ctx)
-          local fname = vim.fn.fnamemodify(item.file, ':~')
-          fname = ctx.width and #fname > ctx.width and vim.fn.pathshorten(fname) or fname
-          if #fname > ctx.width then
-            local dir = vim.fn.fnamemodify(fname, ':h')
-            local file = vim.fn.fnamemodify(fname, ':t')
-            if dir and file then
-              file = file:sub(-(ctx.width - #dir - 2))
-              fname = dir .. '/…' .. file
-            end
-          end
-          local dir, file = fname:match '^(.*)/(.+)$'
-          return dir and { { dir .. '/', hl = 'dir' }, { file, hl = 'file' } } or { { fname, hl = 'file' } }
-        end,
-      },
-      sections = {
-        { section = 'header' },
-        { section = 'keys', gap = 1, padding = 1 },
-        { section = 'startup' },
-      },
-    },
+    dashboard = dashboard,
     indent = { enabled = true }, -- replace indent_line
     input = { enabled = true },
     -- notifier = { enabled = true },  -- replace nvim-notify?
@@ -95,18 +23,18 @@ return {
   },
   keys = {
     {
-      '<leader>z',
+      "<leader>z",
       function()
         Snacks.zen()
       end,
-      desc = 'Toggle Zen Mode',
+      desc = "Toggle Zen Mode",
     },
     {
-      '<leader>Z',
+      "<leader>Z",
       function()
         Snacks.zen.zoom()
       end,
-      desc = 'Toggle Zoom',
+      desc = "Toggle Zoom",
     },
   },
 }
