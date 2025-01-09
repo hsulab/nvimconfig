93f0eedb2212def8acfa4512df3366a287c81e07
jxu <ahcigar@foxmail.com>
2025-01-09 14:43:12 -0500

    codecompanion: disable copilot keymaps

--- lua/plugins/codecompanion.lua
+++ lua/plugins/codecompanion.lua
@@ -21,6 +21,7 @@ return {
     vim.api.nvim_set_keymap("n", "<localleader>c", "<cmd>CodeCompanionChat Toggle<cr>", { silent = true })
 
     -- Set key for completion
+    vim.g.copilot_no_tab_map = true
     vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<CR>")', { noremap = true, silent = true, expr = true })
   end,
 }
