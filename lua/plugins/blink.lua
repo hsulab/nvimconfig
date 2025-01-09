2a61499b1e8ddedb43cd216f884e90c3c8299e70
jxu <ahcigar@foxmail.com>
2025-01-09 12:14:40 -0500

    blink: update keymap

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,5 +1,6 @@
 return {
   "saghen/blink.cmp",
+  event = "InsertEnter",
   -- optional: provides snippets for the snippet source
   dependencies = "rafamadriz/friendly-snippets",
 
@@ -17,13 +18,18 @@ return {
     -- 'super-tab' for mappings similar to vscode (tab to accept, arrow keys to navigate)
     -- 'enter' for mappings similar to 'super-tab' but with 'enter' to accept
     -- See the full "keymap" documentation for information on defining your own keymap.
-    keymap = { preset = "default" },
+    keymap = {
+      preset = "super-tab",
+      cmdline = {
+        preset = "default",
+      },
+    },
 
     appearance = {
       -- Sets the fallback highlight groups to nvim-cmp's highlight groups
       -- Useful for when your theme doesn't support blink.cmp
       -- Will be removed in a future release
-      use_nvim_cmp_as_default = true,
+      use_nvim_cmp_as_default = false,
       -- Set to 'mono' for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
       -- Adjusts spacing to ensure icons are aligned
       nerd_font_variant = "mono",
