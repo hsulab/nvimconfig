118914786810660190e65e0e5dfd2c2212ee3b71
jxu <ahcigar@foxmail.com>
2025-01-09 23:14:53 -0500

    conform: replace ruff with black as a strange bug that does not format `=`

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -34,7 +34,7 @@ return {
       formatters_by_ft = {
         lua = { "stylua" },
         -- Conform can also run multiple formatters sequentially
-        python = { "isort", "ruff" },
+        python = { "isort", "black" },
         rust = { "rustfmt" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
