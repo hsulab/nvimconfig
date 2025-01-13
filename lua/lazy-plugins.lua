9cacf648854e8147029ebe6d702e5ff5b2605ad4
jxu <ahcigar@foxmail.com>
2025-01-13 01:07:10 -0500

    update autopairs

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -37,11 +37,11 @@ require("lazy").setup({
 
   require("plugins/treesitter"),
 
+  require("plugins.autopairs"),
+
   require("plugins/blink"),
 
   -- require("plugins/cmp"),
-
-  -- require("plugins.autopairs"),
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
