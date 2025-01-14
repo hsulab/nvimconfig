afd7484f7b519c1d147976aa12f486e47ed2815b
jxu <ahcigar@foxmail.com>
2025-01-14 00:49:44 -0500

    codesnap: add plugin

---
+++ lua/custom/plugins/codesnap.lua
@@ -0,0 +1,14 @@
+return {
+  "mistricky/codesnap.nvim",
+  build = "make",
+  keys = {
+    { "<leader>sc", "<esc><cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+    { "<leader>ss", "<esc><cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/pictures" },
+  },
+  opts = {
+    save_path = "~/Pictures/codesnap",
+    has_breadcrumbs = true,
+    bg_theme = "bamboo",
+    bg_padding = 0,
+  },
+}
