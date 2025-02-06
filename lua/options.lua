78e77b1f6fcdcf737851e9161bfd8a09f511d11c
jxu <ahcigar@foxmail.com>
2025-02-06 15:33:16 -0500

    render-markdown: replace obsidian ui

--- lua/options.lua
+++ lua/options.lua
@@ -62,7 +62,4 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
--- Obsidian ui for checkboxes
-vim.opt.conceallevel = 2
-
 -- vim: ts=2 sts=2 sw=2 et
