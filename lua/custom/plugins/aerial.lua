fcef89a383b7306fa80f6fbc70f9cf33695d9dd5
jxu <ahcigar@foxmail.com>
2025-02-10 18:38:34 -0500

    aerial: change variable icon as alpha is too large

--- lua/custom/plugins/aerial.lua
+++ lua/custom/plugins/aerial.lua
@@ -1,14 +1,14 @@
 return {
-  'stevearc/aerial.nvim',
+  "stevearc/aerial.nvim",
   -- Optional dependencies
   dependencies = {
-    'nvim-treesitter/nvim-treesitter',
-    'nvim-tree/nvim-web-devicons',
+    "nvim-treesitter/nvim-treesitter",
+    "nvim-tree/nvim-web-devicons",
   },
   opts = {
-    vim.keymap.set('n', '<leader>lS', '<cmd>AerialToggle!<CR>', { desc = 'Symbol Outline' }),
+    vim.keymap.set("n", "<leader>lS", "<cmd>AerialToggle!<CR>", { desc = "Symbol Outline" }),
   },
   config = function()
-    require('aerial').setup { filter_kind = false }
+    require("aerial").setup({ filter_kind = false, icons = { Variable = "󰫧 " } })
   end,
 }
