a0fc7d9bd9988eb96f141c27345c0a96b4e619c4
jxu <ahcigar@foxmail.com>
2025-02-17 14:00:43 -0500

    smartcolumn: add plugin

---
+++ lua/custom/plugins/smartcolumn.lua
@@ -0,0 +1,22 @@
+return {
+  "m4xshen/smartcolumn.nvim",
+  opts = {
+    colorcolumn = "80",
+    disabled_filetypes = {
+      "help",
+      "text",
+      "markdown",
+      "snacks_dashboard",
+      "NvimTree",
+      "lazy",
+      "mason",
+      "checkhealth",
+      "lspinfo",
+      "noice",
+      "Trouble",
+    },
+    custom_colorcolumn = { python = "80", tex = "120" },
+    scope = "file",
+    editorconfig = true,
+  },
+}
