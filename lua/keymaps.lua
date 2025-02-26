786c49f4a0fb5c8259904f86e2a74ca5d8c42ad6
jxu <ahcigar@foxmail.com>
2025-02-26 11:29:20 -0500

    vim-tmux-navigator: update keys

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -33,10 +33,11 @@ vim.keymap.set("n", "<S-l>", "<cmd>bnext<cr>", { desc = "Move focus to the next 
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
-vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
-vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
-vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
+--  Use keys set by vim-tmux-navigator
+-- vim.keymap.set("n", "<C-h>", "<C-w><C-h>", { desc = "Move focus to the left window" })
+-- vim.keymap.set("n", "<C-l>", "<C-w><C-l>", { desc = "Move focus to the right window" })
+-- vim.keymap.set("n", "<C-j>", "<C-w><C-j>", { desc = "Move focus to the lower window" })
+-- vim.keymap.set("n", "<C-k>", "<C-w><C-k>", { desc = "Move focus to the upper window" })
 
 --- Keybinds to close windows and tabs
 vim.keymap.set("n", "<leader>q", "<cmd>q<cr>", { desc = "Close the current window" })
