5b0287b96c6c870d0d8473add0742ba704e88b46
jxu <ahcigar@foxmail.com>
2025-02-27 14:50:37 -0500

    snacks: update image opts

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -1,19 +1,34 @@
 local bufdelete = require("custom.snacks.bufdelete")
 local dashboard = require("custom.snacks.dashboard")
+local image = require("custom.snacks.image")
 local indent = require("custom.snacks.indent")
 local zen = require("custom.snacks.zen")
+
+local keys = {}
+keys = vim.list_extend(keys, bufdelete.keys)
+keys = vim.list_extend(keys, image.keys)
+keys = vim.list_extend(keys, zen.keys)
 
 return {
   "folke/snacks.nvim",
   priority = 1000,
   lazy = false,
   opts = {
+    styles = {
+      relative = "cursor",
+      border = "rounded",
+      focusable = false,
+      backdrop = false,
+      row = 1,
+      col = 1,
+      -- width/height are automatically set by the image size unless specified below
+    },
     -- your configuration comes here
     -- or leave it empty to use the default settings
     -- refer to the configuration section below
     bigfile = { enabled = true },
     dashboard = dashboard.opts,
-    image = { enabled = true },
+    image = image.opts,
     indent = indent.opts, -- replace indent_line
     input = { enabled = true },
     -- notifier = { enabled = true },  -- replace nvim-notify?
@@ -26,5 +41,5 @@ return {
     words = { enabled = true },
     zen = zen.opts,
   },
-  keys = vim.list_extend(zen.keys, bufdelete.keys),
+  keys = keys,
 }
--- lua/custom/snacks/image.lua
+++ lua/custom/snacks/image.lua
@@ -1,4 +1,33 @@
 return {
-  opts = {},
-  keys = {},
+  opts = {
+    enabled = true,
+    doc = {
+      -- enable image viewer for documents
+      -- a treesitter parser must be available for the enabled languages.
+      enabled = true,
+      -- render the image inline in the buffer
+      -- if your env doesn't support unicode placeholders, this will be disabled
+      -- takes precedence over `opts.float` on supported terminals
+      inline = false,
+      -- render the image in a floating window
+      -- only used if `opts.inline` is disabled
+      float = false,
+      max_width = 40,
+      max_height = 20,
+      -- Set to `true`, to conceal the image text when rendering inline.
+      conceal = false, -- (experimental)
+    },
+    math = {
+      enabled = false,
+    },
+  },
+  keys = {
+    {
+      "<localleader>s",
+      function()
+        Snacks.image.hover()
+      end,
+      desc = "Render Image",
+    },
+  },
 }
