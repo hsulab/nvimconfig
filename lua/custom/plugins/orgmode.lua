5d6e632da6cf61dd79fb0a2cca4b34e44d2baddb
jxu <ahcigar@foxmail.com>
2025-02-28 16:30:55 -0500

    orgmode: increase tags_column to 120

--- lua/custom/plugins/orgmode.lua
+++ lua/custom/plugins/orgmode.lua
@@ -7,7 +7,13 @@ return {
     require("orgmode").setup({
       org_agenda_files = "~/Documents/orgfiles/**/*",
       org_default_notes_file = "~/Documents/orgfiles/refile.org",
+      org_tags_column = 120,
       org_capture_templates = {
+        f = {
+          description = "Fixme",
+          template = "* TODO %? %x",
+          target = "~/Documents/orgfiles/fixme.org",
+        },
         r = {
           description = "Repo",
           template = "* [[%x][%(return string.match('%x', '([^/]+)$'))]]%?",
