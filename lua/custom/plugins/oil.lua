401dfac59050695bece1b620c10686a978f57e72
jxu <ahcigar@foxmail.com>
2025-02-27 23:51:54 -0500

    oil: delete plugin

--- lua/custom/plugins/oil.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "stevearc/oil.nvim",
-  opts = {},
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("oil").setup({ default_file_explorer = false })
-    -- vim.api.nvim_set_keymap("n", "<leader>o", "<cmd>Oil --float<cr>", { desc = "Open current directory in Oil" })
-  end,
-}
