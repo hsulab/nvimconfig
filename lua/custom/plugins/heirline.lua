92630cdb39dad72043456fb328df5c2060907db0
jxu <ahcigar@foxmail.com>
2025-02-28 21:46:37 -0500

    heirline: disable winbar for terminal

--- lua/custom/plugins/heirline.lua
+++ lua/custom/plugins/heirline.lua
@@ -1,23 +1,23 @@
 return {
-  'rebelot/heirline.nvim',
-  events = 'VeryLazy',
+  "rebelot/heirline.nvim",
+  events = "VeryLazy",
   config = function()
     vim.opt.cmdheight = 0
-    local conditions = require 'heirline.conditions'
-    require('heirline').setup {
-      statusline = require 'custom.heirline.statusline',
-      tabline = require 'custom.heirline.tabline',
-      winbar = require 'custom.heirline.winbar',
+    local conditions = require("heirline.conditions")
+    require("heirline").setup({
+      statusline = require("custom.heirline.statusline"),
+      tabline = require("custom.heirline.tabline"),
+      winbar = require("custom.heirline.winbar"),
       opts = {
         -- if the callback returns true, the winbar will be disabled for that window
         -- the args parameter corresponds to the table argument passed to autocommand callbacks. :h nvim_lua_create_autocmd()
         disable_winbar_cb = function(args)
           return conditions.buffer_matches({
-            buftype = { 'nofile', 'prompt', 'help', 'quickfix' },
-            filetype = { '^git.*', 'fugitive', 'Trouble', 'dashboard', 'lazygit' },
+            buftype = { "nofile", "prompt", "help", "quickfix", "terminal" },
+            filetype = { "^git.*", "fugitive", "Trouble", "dashboard", "lazygit" },
           }, args.buf)
         end,
       },
-    }
+    })
   end,
 }
