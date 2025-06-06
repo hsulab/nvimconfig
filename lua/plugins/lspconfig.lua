6cf26b9895a57745b486fcf90fe09c8cc7fbf94b
jxu <ahcigar@foxmail.com>
2025-06-05 23:46:25 -0400

    lsp: move gopls

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -180,17 +180,6 @@ return {
       local servers = {
         -- clangd = {},
         fortls = {},
-        gopls = {
-          settings = {
-            gopls = {
-              analyses = {
-                unusedparams = true,
-              },
-              staticcheck = true,
-              gofumpt = true,
-            },
-          },
-        },
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
