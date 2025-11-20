874ccc1a314c72a3ac282819c682f2a576078e97
jxu <ahcigar@foxmail.com>
2025-11-19 22:51:55 -0500

    hop: update key config

--- lua/custom/plugins/hop.lua
+++ lua/custom/plugins/hop.lua
@@ -1,13 +1,22 @@
 return {
   {
-    'smoka7/hop.nvim',
-    event = 'BufEnter',
-    version = '*',
+    "smoka7/hop.nvim",
+    event = "BufEnter",
+    version = "*",
     opts = {},
-    config = function()
-      require('hop').setup {}
-      vim.api.nvim_set_keymap('n', 'W', '<cmd>HopWord<cr>', { silent = true })
-      vim.api.nvim_set_keymap('n', 'E', '<cmd>HopLine<cr>', { silent = true })
-    end,
+    keys = {
+      {
+        "W",
+        "<cmd>HopWord<cr>",
+        desc = "Hop to Word",
+        silent = true,
+      },
+      {
+        "E",
+        "<cmd>HopLine<cr>",
+        desc = "Hop to Line",
+        silent = true,
+      },
+    },
   },
 }
