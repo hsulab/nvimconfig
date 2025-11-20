c148522b608868c64af3abc9af875a18aeb712f5
jxu <ahcigar@foxmail.com>
2025-11-20 17:31:31 -0500

    copilot: move keybind from codecompanion

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,3 +1,8 @@
 return {
   "github/copilot.vim",
+  config = function()
+    -- Set key for completion
+    vim.g.copilot_no_tab_map = true
+    vim.api.nvim_set_keymap("i", "<C-y>", 'copilot#Accept("<CR>")', { noremap = true, silent = true, expr = true })
+  end,
 }
