2a14983c48803e23e516d09771f7b419f5a29649
jxu <ahcigar@foxmail.com>
2025-11-21 14:48:05 -0500

    conform: disable autofmt on lua

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -19,7 +19,8 @@ return {
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true, go = true, python = true, rust = true, typst = true }
+        local disable_filetypes =
+          { c = true, cpp = true, go = true, lua = true, python = true, rust = true, typst = true }
         local lsp_format_opt
         if disable_filetypes[vim.bo[bufnr].filetype] then
           return nil
