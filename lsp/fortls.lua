b56f6f726455225cd8ba5455d1b02aaf13976443
jxu <ahcigar@foxmail.com>
2026-01-06 18:41:55 -0500

    lsp: update fortran

--- lsp/fortls.lua
+++ lsp/fortls.lua
@@ -1,1 +1,12 @@
-return {}
+return {
+  cmd = {
+    "fortls",
+    "--notify_init",
+    "--hover_signature",
+    "--hover_language=fortran",
+    "--use_signature_help",
+  },
+  filetypes = { "fortran" },
+  root_markers = { ".fortls", ".git" },
+  settings = {},
+}
